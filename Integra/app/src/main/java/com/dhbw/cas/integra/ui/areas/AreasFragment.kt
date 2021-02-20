131b34ea9f6c60882c53af634a1009ab97e2809a
AnnalenaF <40786498+AnnalenaF@users.noreply.github.com>
2021-02-20 21:45:42 +0100

    V-F-1_Lebensbereiche: Decorate list with divider

--- Integra/app/src/main/java/com/dhbw/cas/integra/ui/areas/AreasFragment.kt
+++ Integra/app/src/main/java/com/dhbw/cas/integra/ui/areas/AreasFragment.kt
@@ -7,11 +7,14 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dhbw.cas.integra.R
 import com.dhbw.cas.integra.databinding.FragmentAreasBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+
 
 class AreasFragment : Fragment() {
 
@@ -19,22 +22,31 @@ class AreasFragment : Fragment() {
     private lateinit var binding: FragmentAreasBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         areasViewModel =
                 ViewModelProvider(this).get(AreasViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_areas, container, false)
         val root = binding.getRoot()
         //val root = inflater.inflate(R.layout.fragment_areas, container, false)
+
+        // get recycler view containing area list and set adapter
         val recyclerView: RecyclerView = binding.areasList
         val areasAdapter = AreasAdapter()
         areasAdapter.setAreas(areasViewModel.areas)
         recyclerView.adapter = areasAdapter
-        //areasViewModel.text.observe(viewLifecycleOwner, Observer {
-            //textView.text = it
-        //})
+
+        // add divider to recycler view list
+        val layoutManager : LinearLayoutManager = recyclerView.layoutManager as LinearLayoutManager
+        val dividerItemDecoration = DividerItemDecoration(
+            recyclerView.context,
+            layoutManager.getOrientation()
+        )
+        recyclerView.addItemDecoration(dividerItemDecoration)
+
+        // add Listener to Add Button
         val fab: FloatingActionButton = binding.actionAddArea
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
