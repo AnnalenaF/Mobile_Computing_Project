ab66873a17ab5b93e8ec81702257f3f64bda87db
AnnalenaF <40786498+AnnalenaF@users.noreply.github.com>
2021-02-21 16:24:05 +0100

    V-F-1_Lebensbereiche: Enable DB and create Entity
    Area data class is now an entity which can be used for an SQLite DB Table. Dependencies for Room and SQLite have been added.

--- Integra/app/src/main/java/com/dhbw/cas/integra/ui/areas/Area.kt
+++ Integra/app/src/main/java/com/dhbw/cas/integra/ui/areas/Area.kt
@@ -1,3 +1,11 @@
 package com.dhbw.cas.integra.ui.areas
 
-data class Area(var text: String, var label: Int)
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="areas")
+data class Area(
+    var text: String,
+    var label: Int,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0
+)
