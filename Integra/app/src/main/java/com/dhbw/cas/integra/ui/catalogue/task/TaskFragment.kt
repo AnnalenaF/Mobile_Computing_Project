a117d51cdf3709f0527824a95fba03723ec5c492
AnnalenaF <40786498+AnnalenaF@users.noreply.github.com>
2021-02-25 00:34:03 +0100

    V-F-2_Aufgabenkatalog: Enable edit mode for task detail
    Switch between edit mode and display mode is enabled, as well as cancel editing. To be done: saving the changes
    
    Refers to V-F-2.2

--- Integra/app/src/main/java/com/dhbw/cas/integra/ui/catalogue/task/TaskFragment.kt
+++ Integra/app/src/main/java/com/dhbw/cas/integra/ui/catalogue/task/TaskFragment.kt
@@ -3,9 +3,7 @@ package com.dhbw.cas.integra.ui.catalogue.task
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.InputType
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
@@ -16,15 +14,24 @@ import androidx.navigation.fragment.navArgs
 import com.dhbw.cas.integra.R
 import com.dhbw.cas.integra.ui.areas.AreasViewModel
 import com.google.android.material.textview.MaterialTextView
-import kotlinx.android.synthetic.main.dialog_new_task.*
+
 
 class TaskFragment : Fragment() {
     private lateinit var areasViewModel: AreasViewModel
     private lateinit var backgroundEdit: Drawable
+    private var editMode: Boolean = false
+    private lateinit var title: EditText
+    private lateinit var areaSpinner : Spinner
+    private lateinit var areaDisplay : MaterialTextView
+    private lateinit var areaLabel : MaterialTextView
+    private lateinit var priority : EditText
+    private lateinit var expectedDuration : EditText
+    private lateinit var description : EditText
+    private lateinit var args: TaskFragmentArgs
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_task, container, false)
         areasViewModel =
@@ -33,17 +40,22 @@ class TaskFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val title: EditText = view.findViewById(R.id.task_title)
-        val areaSpinner : Spinner = view.findViewById(R.id.task_area_spinner)
-        val areaDisplay : MaterialTextView = view.findViewById(R.id.task_area)
-        val priority : EditText = view.findViewById(R.id.task_prio)
-        val expectedDuration : EditText = view.findViewById(R.id.task_duration)
-        val description : EditText = view.findViewById(R.id.task_descr)
-        val args: TaskFragmentArgs by navArgs()
+        title = view.findViewById(R.id.task_title)
+        areaSpinner = view.findViewById(R.id.task_area_spinner)
+        areaDisplay = view.findViewById(R.id.task_area)
+        priority = view.findViewById(R.id.task_prio)
+        expectedDuration = view.findViewById(R.id.task_duration)
+        description = view.findViewById(R.id.task_descr)
+        val argsLocal: TaskFragmentArgs by navArgs()
+        args = argsLocal
         title.setText(args.title)
         // set adapter for areas spinner
         areasViewModel.getAreaTexts().observe(viewLifecycleOwner, { spinnerData ->
-            val spinnerAdapter = ArrayAdapter(view.context, android.R.layout.simple_spinner_item, spinnerData)
+            val spinnerAdapter = ArrayAdapter(
+                view.context,
+                android.R.layout.simple_spinner_item,
+                spinnerData
+            )
             areaSpinner.adapter = spinnerAdapter
         })
         // get area of current task and set it as selected
@@ -54,24 +66,20 @@ class TaskFragment : Fragment() {
                 areaSpinner.setSelection(i)
             }
         }*/
-        areaDisplay.setText(args.areaText)
-        areaDisplay.setBackgroundResource(args.areaLabel)
-        priority.setText(args.priority.toString())
-        expectedDuration.setText(args.expectedDuration.toString())
-        description.setText(args.description)
+        setValuesFromArgs()
 
         // disable editing
-        val areaLabel : MaterialTextView = view.findViewById(R.id.task_area_label)
+        areaLabel = view.findViewById(R.id.task_area_label)
         areaLabel.isVisible = false
         areaSpinner.isVisible = false
         backgroundEdit = title.background
-        switchEditMode(title)
-        switchEditMode(priority)
-        switchEditMode(expectedDuration)
-        switchEditMode(description)
+        switchEditModeForText(title)
+        switchEditModeForText(priority)
+        switchEditModeForText(expectedDuration)
+        switchEditModeForText(description, true)
     }
 
-    private fun switchEditMode(editText: EditText) {
+    private fun switchEditModeForText(editText: EditText, multiline: Boolean = false) {
         if (editText.inputType != InputType.TYPE_NULL){
             editText.apply {
                 inputType = InputType.TYPE_NULL
@@ -83,7 +91,14 @@ class TaskFragment : Fragment() {
             }
         } else {
             editText.apply {
-                inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+                if (multiline) {
+                    inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_FLAG_MULTI_LINE
+                    isSingleLine = false
+                    minLines = 1
+                    maxLines = 10
+                } else {
+                    inputType = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+                }
                 isClickable = true
                 isCursorVisible = true
                 isFocusable = true
@@ -91,5 +106,85 @@ class TaskFragment : Fragment() {
                 background = backgroundEdit
             }
         }
+    }
+
+    // enable fragment to display options menu
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.edit_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        val editAction = menu.findItem(R.id.action_edit)
+        val cancelAction = menu.findItem(R.id.action_cancel)
+        val saveAction = menu.findItem(R.id.action_save)
+        val settings = menu.findItem(R.id.action_settings)
+        settings.isVisible = false
+        if (editMode){
+            editAction.isVisible = false
+            cancelAction.isVisible = true
+            saveAction.isVisible = true
+        } else {
+            editAction.isVisible = true
+            cancelAction.isVisible = false
+            saveAction.isVisible = false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_edit -> {
+                switchEditMode()
+                true
+            }
+            R.id.action_save -> {
+                // TODO validate
+                // TODO save
+                switchEditMode()
+                true
+            }
+            R.id.action_cancel -> {
+                setValuesFromArgs()
+                switchEditMode()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun switchEditMode() {
+        if (editMode) {
+            editMode = false
+            areaLabel.isVisible = false
+            areaSpinner.isVisible = false
+            areaDisplay.isVisible = true
+            switchEditModeForText(title)
+            switchEditModeForText(priority)
+            switchEditModeForText(expectedDuration)
+            switchEditModeForText(description, true)
+        } else {
+            editMode = true
+            areaLabel.isVisible = true
+            areaSpinner.isVisible = true
+            areaDisplay.isVisible = false
+            switchEditModeForText(title)
+            switchEditModeForText(priority)
+            switchEditModeForText(expectedDuration)
+            switchEditModeForText(description, true)
+        }
+        requireActivity().invalidateOptionsMenu()
+    }
+
+    private fun setValuesFromArgs() {
+        areaDisplay.setText(args.areaText)
+        areaDisplay.setBackgroundResource(args.areaLabel)
+        priority.setText(args.priority.toString())
+        expectedDuration.setText(args.expectedDuration.toString())
+        description.setText(args.description)
     }
 }
