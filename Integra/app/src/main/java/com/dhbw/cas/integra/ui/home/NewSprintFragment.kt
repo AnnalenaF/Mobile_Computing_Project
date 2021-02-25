ee4034578023222e67174b908583e6794a287e80
AnnalenaF <40786498+AnnalenaF@users.noreply.github.com>
2021-02-25 18:20:15 +0100

    V-F-3_Sprintplanung: Display calendar to pick duration of sprint
    External library has been reused for the date range picker. Formatting has been adjusted to match the color theme. Default duration of sprint is preselected. Validation of start date and duration of sprint has been added when leaving page.
    
    Refers to V-F-3.2 and V-F-3.3

--- Integra/app/src/main/java/com/dhbw/cas/integra/ui/home/NewSprintFragment.kt
+++ Integra/app/src/main/java/com/dhbw/cas/integra/ui/home/NewSprintFragment.kt
@@ -1,13 +1,20 @@
 package com.dhbw.cas.integra.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.dhbw.cas.integra.R
+import com.google.android.material.snackbar.Snackbar
+import com.savvi.rangedatepicker.CalendarPickerView
+import kotlinx.android.synthetic.main.fragment_new_sprint.*
+import java.util.*
 
 class NewSprintFragment: Fragment() {
     private lateinit var root: View
+    private lateinit var today: Date
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +22,26 @@ class NewSprintFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         root = inflater.inflate(R.layout.fragment_new_sprint, container, false)
+
+        // instantiate calender to pick sprint duration
+        val calendar = Calendar.getInstance()
+        // clear time information for comparability of dates
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+        today = calendar.time
+        //default sprint duration is 2 weeks = 14 days
+        calendar.add(Calendar.DAY_OF_YEAR, 13)
+        val defaultSprintEnd = calendar.time
+        //maximum sprint duration is 1 year = 365 days
+        calendar.add(Calendar.DAY_OF_YEAR, 364)
+        val maxDate = calendar.time
+        val calendarView = root.findViewById<CalendarPickerView>(R.id.calendar_sprint_duration)
+        calendarView.init(today, maxDate)
+            .inMode(CalendarPickerView.SelectionMode.RANGE)
+            .withSelectedDates(listOf<Date>(today, defaultSprintEnd))
+
         return root
     }
 
@@ -37,8 +64,20 @@ class NewSprintFragment: Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_continue -> { // continue to task assignment
-                val action = NewSprintFragmentDirections.actionNavNewSprintToNavNewSprintTasks()
-                root.findNavController().navigate(action)
+                val selectedDates = calendar_sprint_duration.selectedDates
+                if (selectedDates[0] != today){
+                    Snackbar.make(root, R.string.sprint_start_error, Snackbar.LENGTH_LONG)
+                        .setTextColor(ContextCompat.getColor(root.context, R.color.red_error))
+                        .show()
+                } else if (selectedDates.size < 5){
+                    Snackbar.make(root, getString(R.string.sprint_length_error, 5),
+                                  Snackbar.LENGTH_LONG)
+                        .setTextColor(ContextCompat.getColor(root.context, R.color.red_error))
+                        .show()
+                } else {
+                    val action = NewSprintFragmentDirections.actionNavNewSprintToNavNewSprintTasks()
+                    root.findNavController().navigate(action)
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
